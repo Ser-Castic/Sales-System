b5f96dadb9498db31a24234bd90b4ce472acbdec
Ser-Castic <samsteinberg90@gmail.com>
2017-01-13 17:37:56 -0500

    Trying to figure out the spark code to show home html

--- src/com/theironyard/charlotte/Main.java
+++ src/com/theironyard/charlotte/Main.java
@@ -1,8 +1,73 @@
 package com.theironyard.charlotte;
 
+import jodd.json.JsonSerializer;
+import org.h2.tools.Server;
+import spark.ModelAndView;
+import spark.Session;
+import spark.Spark;
+import spark.template.mustache.MustacheTemplateEngine;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void createTables(Connection conn) throws SQLException {
+        Statement stmt = conn.createStatement();
+        stmt.execute("CREATE TABLE IF NOT EXISTS users (id IDENTITY, username VARCHAR, email VARCHAR)");
+        stmt.execute("CREATE TABLE IF NOT EXISTS items (id IDENTITY, item_name VARCHAR, item_quantity INT, item_price DOUBLE, order_id INT)");
+        stmt.execute("CREATE TABLE IF NOT EXISTS orders (id IDENTITY, user_id INT)");
+    }
+
+    public static void main(String[] args) throws SQLException {
+        try {
+            Server.createWebServer().start();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        Connection conn = DriverManager.getConnection("jdbc:h2:./main");
+        createTables(conn);
+
+//        Spark.externalStaticFileLocation("public");
+        Spark.init();
+
+        Spark.get(
+                "/",
+                ((request, response) -> {
+                    HashMap m = new HashMap();
+
+                    Session session = request.session();
+                    Integer userId = session.attribute("userId");
+
+                    User currentUser = User.selectUserById(conn, new User(userId));
+
+                    if (currentUser == null) {
+                        return new ModelAndView(m, "login.html");
+                    } else {
+                        m.put("name", currentUser.userName);
+                        m.put("email", currentUser.userEmail);
+                        return new ModelAndView(m, "home.html");
+                    }
+                }),
+                new MustacheTemplateEngine()
+        );
+
+        Spark.post(
+                "/login",
+                (request, response) -> {
+                    String name = request.queryParams("loginName");
+                    String email = request.queryParams("loginEmail");
+                    User.insertUser(conn, new User(name, email));
+                    User user = User.selectUserByNameAndEmail(conn, new User(name, email));
+
+                    Session session = request.session();
+                    session.attribute("id", user.getUserId());
+                    session.attribute("userName", user.getUserName());
+                    session.attribute("userEmail", user.getUserEmail());
+
+                    response.redirect("/");
+                    return "";
+                });
     }
 }
--- src/com/theironyard/charlotte/User.java
+++ src/com/theironyard/charlotte/User.java
@@ -1,5 +1,9 @@
 package com.theironyard.charlotte;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class User {
@@ -11,11 +15,19 @@ public class User {
     public User() {
     }
 
-    public User(Integer userId, String userName, String userEmail, ArrayList<Order> userOrders) {
+    public User(Integer userId) {
+        this.userId = userId;
+    }
+
+    public User(String userName, String userEmail) {
+        this.userName = userName;
+        this.userEmail = userEmail;
+    }
+
+    public User(Integer userId, String userName, String userEmail) {
         this.userId = userId;
         this.userName = userName;
         this.userEmail = userEmail;
-        this.userOrders = userOrders;
     }
 
     public Integer getUserId() {
@@ -48,6 +60,45 @@ public class User {
 
     public void setUserOrders(ArrayList<Order> userOrders) {
         this.userOrders = userOrders;
+    }
+
+
+    public static User selectUserById(Connection conn, User currentUser) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("SELECT * FROM users WHERE id = ?");
+        stmt.setInt(1, currentUser.getUserId());
+
+        ResultSet results = stmt.executeQuery();
+        if (results.next()) {
+            return makeUser(results);
+        } else {
+            return null;
+        }
+    }
+
+    public static User makeUser(ResultSet results) throws SQLException{
+        int id = results.getInt("id");
+        String userName = results.getString("username");
+        String email = results.getString("email");
+        return new User(id, userName, email);
+    }
+
+    public static User selectUserByNameAndEmail(Connection conn, User currentUser) throws SQLException{
+        PreparedStatement stmt = conn.prepareStatement("SELECT * FROM users WHERE upper(username) = ? and upper(email) = ?");
+        stmt.setString(1, currentUser.getUserName().toUpperCase());
+        stmt.setString(2, currentUser.getUserEmail().toUpperCase());
+        ResultSet results = stmt.executeQuery();
+        if(results.next()) {
+            return makeUser(results);
+        } else {
+            return null;
+        }
+    }
+
+    public static void insertUser(Connection conn, User newUser) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("INSERT INTO users VALUES (NULL, ?, ?)");
+        stmt.setString(1, newUser.getUserName());
+        stmt.setString(2, newUser.getUserEmail());
+        stmt.execute();
     }
 }
 
